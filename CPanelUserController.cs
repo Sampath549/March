d7f7d0a4f7ecfdac544d994ae363cefae68eea04
Sampath549 <45259400+Sampath549@users.noreply.github.com>
2019-03-15 06:52:56 +0530

    Add files via upload

---
+++ CPanelUserController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Filters;
+using WebApp.Helper;
+using WebApp.Models.SharedEntities;
+
+namespace WebApp.Controllers
+{
+    [MVCSessionFilter, MVCUserAuthorization]
+    public class CPanelUserController : Controller
+    {
+        #region Global Variables
+        SE_Users _SessionUserDetails = SessionHandler.UserDetails as SE_Users;
+        #endregion
+
+        #region Dashboard
+        public ActionResult Dashboard()
+        {
+            try
+            {
+                ViewBag.DisplayModalPopup = Reusable.CheckIsFirstTimeLogin();   // Model PopUp   
+                ViewBag.Menus = Reusable.BindMenus();   // Bind Menus
+            }
+            catch (WebException ex)
+            {
+                return RedirectToAction(StatusCode.ErrorPage(ex), "Error");
+            }
+            return View();
+        }
+        #endregion
+    }
+}
---
+++ MVCUserAuthorization.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using WebApp.Helper;
+using WebApp.Models.SharedEntities;
+
+namespace WebApp.Filters
+{
+    public class MVCUserAuthorization : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            HttpContext ctx = HttpContext.Current;
+
+            SE_Users Users = SessionHandler.UserDetails as SE_Users;
+            if (Users.RoleCode != GlobalVariables.Shared.UserRole)
+            {
+                var values = new RouteValueDictionary(new { action = GlobalVariables.Shared.Unauthorized, controller = "Error" });
+                filterContext.Result = new RedirectToRouteResult(values);
+                return;
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
